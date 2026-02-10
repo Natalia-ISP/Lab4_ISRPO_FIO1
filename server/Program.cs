986f6bcd5e56c35ae87de8da18f16dc239738b0e
natalia <ponomarevanatasha16072007@gmail.com>
2026-02-10 17:49:33 +0300

    добавление меню

--- server/Program.cs
+++ server/Program.cs
@@ -8,6 +8,17 @@ class Program
         string name = "Пономарева Наталья Андреевна";
         string group = "ИСП-232";
         DateTime a = DateTime.Now;
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("\nМеню:");
+            Console.WriteLine("1 — Показать ФИО");
+            Console.WriteLine("2 — Показать группу");
+            Console.WriteLine("3 — Показать дату");
+            Console.WriteLine("4 — Выход");
+            Console.Write("Введите номер пункта: ");
+        }
         
     }
 }
